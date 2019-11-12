29af324212cc450b155f0084957fad7197825761
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-11-12 23:09:08 +0100

    EXPERIMENT: add a GitHub Action to setup git-sdk-64-minimal
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ setup-git-sdk-64-minimal/index.js
@@ -0,0 +1,36 @@
+import * as core from '@actions/core';
+import * as io from '@actions/io';
+import * as toolcache from '@actions/tool-cache';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as unzipper from 'unzipper';
+
+async function run() {
+  try {
+    if (process.platform !== 'win32') {
+      core.setFailed("git-sdk-64-minimal needs to run on Windows");
+      return;
+    }
+
+    const tmp = process.env['RUNNER_TEMP'];
+    if (!tmp) {
+      core.setFailed('environment variable RUNNER_TEMP is undefined');
+      return;
+    }
+    // TODO: obtain this URL dynamically
+    const url = 'https://artifacts.dev.azure.com/Git-for-Windows/f3317b6a-fa67-40d4-9a33-b652e06943df/_apis/artifact/cGlwZWxpbmVhcnRpZmFjdDovL0dpdC1mb3ItV2luZG93cy9wcm9qZWN0SWQvZjMzMTdiNmEtZmE2Ny00MGQ0LTlhMzMtYjY1MmUwNjk0M2RmL2J1aWxkSWQvNDU1NDIvYXJ0aWZhY3ROYW1lL2dpdC1zZGstNjQtbWluaW1hbA2/content?format=zip';
+    const zip = await toolcache.downloadTool(url);
+
+    core.startGroup('Unzipping git-sdk-64-mininal...');
+    const path = core.getInput('path');
+    await io.mkdirP(path);
+    fs.createReadStream(zip).pipe(unzipper.Extract({path: path}));
+    core.endGroup();
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
+
